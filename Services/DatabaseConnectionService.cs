f755f884fd09c9951ec1cdc2c174d9a022fd1892
Karl Antonio <antonio.karlvincent@auf.edu.ph>
2024-10-23 06:27:43 +0800

    Add project files.

---
+++ Services/DatabaseConnectionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Module07DataAccess.Services
+{
+    public class DatabaseConnectionService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnectionService()
+        {
+
+            _connectionString = "Server=localhost;Database=CompanyDB;User ID=employeeuser;Password=employeeuser";
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
---
+++ ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Module07DataAccess.Model;
+using Module07DataAccess.Services;
+
+namespace Module07DataAccess.ViewModel
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private readonly EmployeeService _employeeService;
+        public ObservableCollection<Employee> EmployeeList { get; set; }
+        private string _statusMessage;
+
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoadEmployeesCommand { get; }
+
+        public EmployeeViewModel()
+        {
+            _employeeService = new EmployeeService();
+            EmployeeList = new ObservableCollection<Employee>();
+            LoadEmployeesCommand = new Command(async () => await LoadEmployees());
+        }
+
+        public async Task LoadEmployees()
+        {
+            StatusMessage = "Loading employees...";
+            try
+            {
+                var employees = await _employeeService.GetEmployeesAsync();
+                EmployeeList.Clear();
+                foreach (var employee in employees)
+                {
+                    EmployeeList.Add(employee);
+                }
+                StatusMessage = "Employees loaded successfully!";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to load employees: {ex.Message}";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
